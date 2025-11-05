efc5a8a250c6b4f1a5173430f139c68cada0d443
Oluwatosin Olayinka <demonslayerguy@gmail.com>
2025-11-04 21:00:41 -0500

    Add files via upload

---
+++ pam_to_vcf.py
@@ -0,0 +1,60 @@
+import argparse
+from EIGENTOOLS import PackedAncestryMap
+from math import nan
+
+
+def flatten(xss):
+    """
+    flattens a list of lists into a list of elements
+    """
+    return [x for xs in xss for x in xs]
+
+
+def unique(sequence):
+    """
+    Gets all of the unique elements of a list while maintianing the original order
+    """
+    seen = set()
+    return [x for x in sequence if not (x in seen or seen.add(x))]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument("--file_prefix", help="PACKEDANCESTRYMAP file prefix")
+    parser.add_argument("--pops", help="File with newline-separated list of populations to include")
+    parser.add_argument("--flip", help="Flip alleles and adjust dosage",
+                        action='store_true')
+
+    args = parser.parse_args()
+
+    print("##fileformat=VCFv4.2")
+    print("##FILTER=<ID=PASS,Description=\"All filters passed\">")
+
+    pam = PackedAncestryMap(file_prefix=args.file_prefix)
+    # get pops to include
+    pops = []
+    with open(args.pops) as f:
+        for line in f:
+            pops.append(line.strip())
+
+    inds = flatten([pam.ind_info.get_label_indices(pop) for pop in pops])
+
+    for chrom in unique(pam.snp_info.chrom):
+        print("##contig=<ID=%s>" % chrom)
+
+    print("##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">")
+    # print header
+    print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t", end="")
+    print("\t".join([pam.ind_info.ind_name[i] for i in inds]))
+    map_dosage_normal = {0: "\t0/0", 1: "\t0/1", 2: "\t1/1", nan: "\t./."}
+    map_dosage_flipped = {2: "\t0/0", 1: "\t0/1", 0: "\t1/1", nan: "\t./."}
+    map_dosage = map_dosage_flipped if args.flip else map_dosage_normal
+
+    for e in pam:
+        sinfo = e.get_SNP_Info()
+        print("\t".join([sinfo.chrom[0], "%i" % sinfo.pos[0], sinfo.var_name[0],
+                         sinfo.ref[0], sinfo.alt[0], "999", "PASS", ".", "GT"]), end="")
+        for c in [e.geno[i] for i in inds]:
+            print(map_dosage[c], end="")
+        print("")
---
+++ vcf_to_pam.py
@@ -0,0 +1,94 @@
+from EIGENTOOLS import SNP_Info, Ind_Info, PackedAncestryMapWriter
+import gzip
+import argparse
+
+
+GT_MAP = {"0/0": 0, "1/0": 1, "0/1": 1, "1/1": 2}
+
+
+def write_snp_ind(vcf_file, snp_out, ind_out, ind_label, ind_sex=None):
+    f = None
+    if vcf_file.endswith(".gz"):
+        f = gzip.open(vcf_file, "rt")
+    else:
+        f = open(vcf_file)
+
+    f_out_snp = open(snp_out, "w+")
+    f_out_ind = open(ind_out, "w+")
+
+    ind_name = None
+    ind_sex = ["U"] * len(ind_label) if ind_sex is None else ind_sex
+    for line in f:
+        if line.startswith("#CHROM"):
+            # write ind file
+            ind_name = line.split()[9:]
+            if not (len(ind_sex) == len(ind_label) == len(ind_name)):
+                raise Exception("Length mismatch between individual file information")
+            for name, sex, label in zip(ind_name, ind_sex, ind_label):
+                f_out_ind.write("%s\t%s\t%s\n" % (name, sex, label))
+            f_out_ind.close()
+            continue
+        elif line.startswith("#"):
+            continue
+
+        elems = line.split()
+        chrom = elems[0].replace("chr", "")  # remove chr symbol
+        pos = elems[1]
+        ref = elems[3]
+        alt = elems[4]
+        # skip if multillelic
+        if "," in alt:
+            continue
+        snp_id = elems[2] if elems[2].startswith("rs") else "_".join(["snp", chrom, pos])
+        f_out_snp.write("\t".join([snp_id, chrom, "0.0", pos, ref, alt]) + "\n")
+    f_out_snp.close()
+    f.close()
+
+
+def vcf_to_pam(vcf_file, pam_prefix, ind_label, ind_sex=None, reverse=False):
+    # write snp and ind files from the VCF
+    write_snp_ind(vcf_file, pam_prefix + ".snp", pam_prefix + ".ind",
+                  ind_label, ind_sex)
+
+    snp_info = SNP_Info(pam_prefix + ".snp")
+    ind_info = Ind_Info(pam_prefix + ".ind")
+
+    paw = PackedAncestryMapWriter(snp_info, ind_info, file_prefix=pam_prefix,
+                                  write_snp=False, write_ind=False)
+
+    f = None
+    if vcf_file.endswith(".gz"):
+        f = gzip.open(vcf_file, "rt")
+    else:
+        f = open(vcf_file)
+
+    for line in f:
+        if line.startswith("#"):
+            continue
+        elems = line.split()
+        alt = elems[4]
+        # skip if multiallelic
+        if "," in alt:
+            continue
+        fmt_info = elems[8].split(":")
+        gt_idx = fmt_info.index("GT")
+        genotypes = [x.split(":")[gt_idx].replace("|", "/") for x in elems[9:]]
+        dosages = [GT_MAP[x] for x in genotypes]
+        if reverse:
+            dosages = [2 - x for x in dosages]
+        paw.write_record(dosages)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument("--vcf", help="VCF file input")
+    parser.add_argument("--out_prefix", help="output PACKEDANCESTRYMAP file prefix")
+    parser.add_argument("--ind_pop", nargs="*", default=None, help="Population labels for each of the individuals in the VCF file")
+    parser.add_argument("--ind_sex", nargs="*", default=None, help="Sex for each of the individuals in the VCF file")
+    parser.add_argument("--flip", help="Flip alleles and adjust dosage",
+                        action='store_true')
+
+    args = parser.parse_args()
+
+    vcf_to_pam(args.vcf, args.out_prefix, args.ind_pop, args.ind_sex, args.flip)
